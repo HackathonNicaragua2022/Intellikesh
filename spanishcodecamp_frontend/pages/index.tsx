62d52e77626cbf7c6f5939f11e07bdb716875c30
vidalbaquedano <vidalbaquedano@gmail.com>
2022-10-22 21:50:28 -0600

    index-fix

--- spanishcodecamp_frontend/pages/index.tsx
+++ spanishcodecamp_frontend/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 import { useSetRecoilState } from "recoil";
-import { testAtom } from "../state/atoms";
 const Home: NextPage = () => {
-  const setTest = useSetRecoilState(testAtom);
   return (
     <div className="flex items-center justify-center">
       <h1 className="text-6xl">HELLO WORLD</h1>
