3ecedcaf39e641785ac013a900650e83dc31ef58
Osc100 <oscar.javier.marin.robleto@gmail.com>
2022-10-22 21:26:41 -0600

    Added the register, and login logic services in the api
    - The login service is a POST service that requires username and password
    
    - The register service also requires both names and the email.
    
    - Integrated knox tokens for the authentication
    
    - Added CORS headers

--- spanishcodecamp_backend/spanishcodecamp_backend/settings.py
+++ spanishcodecamp_backend/spanishcodecamp_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,13 +26,15 @@ SECRET_KEY = "django-insecure-vm6@lw4&d-h#j$26xbbvn70y4_(@(i*fzra%*r$khb-!+$2%q_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     "api",
+    "knox",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -123,3 +127,22 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "api.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "knox.auth.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+}
+
+REST_KNOX = {"AUTH_HEADER_PREFIX": "Bearer", "TOKEN_TTL": timedelta(days=2)}
+LANGUAGE_CODE = "es-NI"
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+]
+
+
+# CORS, REMOVE IN PRODUCTION
+CORS_ALLOW_ALL_ORIGINS = True
