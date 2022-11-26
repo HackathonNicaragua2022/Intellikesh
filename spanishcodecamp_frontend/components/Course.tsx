c2e6a60e455d62b15ded7d69fade9136d290ae04
vidalbaquedano <vidalbaquedano@gmail.com>
2022-11-26 08:06:42 -0600

    course

--- spanishcodecamp_frontend/components/Course.tsx
+++ spanishcodecamp_frontend/components/Course.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import CourseCard from "./CourseCard";
+import One from "assets/img/Python.png";
 
 const Course = () => {
-  return <div>Course</div>;
+  const courses = [
+    {
+      image: One,
+      level: "10 niveles restantes",
+      label: "Python Avanzado",
+      descripton:
+        "La programación funcional es otro enfoque popular para el desarrollo de software, el código se organiza en...",
+    },
+    {
+      image: One,
+      level: "10 niveles restantes",
+      label: "Python Avanzado",
+      descripton:
+        "La programación funcional es otro enfoque popular para el desarrollo de software, el código se organiza en...",
+    },
+    {
+      image: One,
+      level: "10 niveles restantes",
+      label: "Python Avanzado",
+      descripton:
+        "La programación funcional es otro enfoque popular para el desarrollo de software, el código se organiza en...",
+    },
+    {
+      image: One,
+      level: "10 niveles restantes",
+      label: "Python Avanzado",
+      descripton:
+        "La programación funcional es otro enfoque popular para el desarrollo de software, el código se organiza en...",
+    },
+  ];
+
+  return (
+    <div className="px-10">
+      <header className="text-white text-[2rem] flex justify-between items-center h-[160px] py-4 w-full">
+        <span className="text-textColor">Información de Cursos</span>
+      </header>
+      <section className="grid grid-cols-3 gap-8 w-[100%] h-[600px] bg-assent p-[2.5rem] rounded-xl">
+        {courses.map((course, index) => (
+          <CourseCard
+            key={index}
+            label={course.label}
+            level={course.level}
+            description={course.descripton}
+            image={course.image}
+          />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Course;
--- spanishcodecamp_frontend/components/CourseCard.tsx
+++ spanishcodecamp_frontend/components/CourseCard.tsx
@@ -10,12 +10,12 @@ interface CourseCard {
 
 const CourseCard: FC<CourseCard> = ({ image, label, description, level }) => {
   return (
-    <div className="flex flex-col cursor-pointer text-white w-[300px] h-[250px] bg-complement rounded-xl p-4 gap-4">
+    <div className="flex flex-col cursor-pointer text-textColor w-[300px] h-[250px] bg-complement rounded-xl p-4 gap-4">
       <section className="flex items-center justify-between text-center">
         <div className="relative w-[70px] h-[70px] rounded-xl bg-black">
           <Image src={image} alt="Image" layout="fill" />
         </div>
-        <span className="text-[1.5rem]">{label}</span>
+        <span className="text-[1.5rem] text-white">{label}</span>
       </section>
       <section>
         <span>{level}</span>
