f0cd526468c81c783af0e0e28f1bedacc5ad25c0
Osc100 <oscar.javier.marin.robleto@gmail.com>
2022-11-26 13:36:05 -0600

    Added page title

--- spanishcodecamp_frontend/pages/_app.tsx
+++ spanishcodecamp_frontend/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import NonFieldErrorCatcher from "../components/NonFieldErrorCatcher";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Head>
+        <title>Spanish Code Camp</title>
+      </Head>
       <NonFieldErrorCatcher />
       <Component {...pageProps} />
     </RecoilRoot>
