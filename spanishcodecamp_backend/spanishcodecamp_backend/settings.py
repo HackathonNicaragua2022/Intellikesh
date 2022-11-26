dd9577ef7702ab35d8c912e730a40c3658f83c1b
Osc100 <oscar.javier.marin.robleto@gmail.com>
2022-11-26 12:37:44 -0600

    changed language code again

--- spanishcodecamp_backend/spanishcodecamp_backend/settings.py
+++ spanishcodecamp_backend/spanishcodecamp_backend/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "es-NI"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "America/Managua"
 
